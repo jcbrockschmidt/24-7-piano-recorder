02a2e213ba5c6f4dbf861aab2623bb2ad4a69814
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2023-12-25 23:18:42 -0800

    Add bit depth to config and use dBFS for volume

--- listen.py
+++ listen.py
@@ -12,8 +12,20 @@ import wave
 
 CONFIG_PATH = 'config.toml'
 
-FORMAT = pyaudio.paInt16
-CHUNK_SIZE = 512
+NUM_FRAMES = 512
+
+BIT_DEPTH_TO_FORMAT = {
+    8: pyaudio.paInt8,
+    16: pyaudio.paInt16,
+    32: pyaudio.paInt32,
+}
+
+BIT_DEPTH_TO_NP_DTYPE = {
+    8: np.int8,
+    16: np.int16,
+    # We exclude 24 bits because numpy does not have a dtype for it.
+    32: np.int32
+}
 
 # TODO: use later if necessary
 # HZ_LOWPASS = 27.5
@@ -27,15 +39,19 @@ def get_save_path(dt, parent_dir):
     save_name = dt.astimezone().strftime('%Y-%m-%d %H:%M:%S %Z.wav')
     return path.join(path.realpath(parent_dir), save_name)
 
-def get_rms_db_for_chunk(data, width):
+def get_rms_dbfs_for_chunk(data, width):
     rms = audioop.rms(data, width)
-    return 0 if rms == 0 else 20 * log10(rms)
+    # If rms is 0, the dBFS value is undefined.
+    rms = 1 if rms == 0 else rms
+    max_signal = 2 ** (8 * width - 1) - 1
+    # https://dsp.stackexchange.com/questions/8785/how-to-compute-dbfs
+    return 20 * log10(abs(rms) / max_signal)
 
-def save_frames(frames, channels, sample_rate, save_path):
+def save_frames(frames, channels, sample_rate, format, save_path):
     audio = pyaudio.PyAudio()
     with wave.open(save_path, 'wb') as f:
         f.setnchannels(channels)
-        f.setsampwidth(audio.get_sample_size(FORMAT))
+        f.setsampwidth(audio.get_sample_size(format))
         f.setframerate(sample_rate)
         f.writeframes(frames)
 
@@ -45,48 +61,59 @@ def listen(config):
     rec_cfg = config['recording']
     output_cfg = config['output']
 
+    bit_depth = input_cfg['bit_depth']
+    pyaudio_format = BIT_DEPTH_TO_FORMAT[bit_depth]
+    bit_depth_dtype = BIT_DEPTH_TO_NP_DTYPE[bit_depth]
+    channels = input_cfg['channels']
+    sample_rate = input_cfg['sample_rate']
+
     audio = pyaudio.PyAudio()
 
     device_name = audio.get_device_info_by_index(input_cfg['device_index']).get('name')
     print(f'Using device "{device_name}"')
+    print(f'Sample rate {sample_rate} Hz')
+    print(f'Bit depth {bit_depth}')
+    print(f'{channels} channels')
+    print()
+
 
     stream = audio.open(
         input=True,
         input_device_index=input_cfg['device_index'],
-        format=FORMAT,
-        channels=input_cfg['channels'],
-        rate=input_cfg['sample_rate'],
-        frames_per_buffer=CHUNK_SIZE,
+        format=pyaudio_format,
+        channels=channels,
+        rate=sample_rate,
+        frames_per_buffer=NUM_FRAMES,
     )
 
-    width = audio.get_sample_size(FORMAT)
-    buffer = np.empty((input_cfg['channels'], 0), np.int16)
-    db_samples = np.empty((input_cfg['channels'], 0), np.double)
+    width = audio.get_sample_size(pyaudio_format)
+    buffer = np.empty((channels, 0), bit_depth_dtype)
+    db_samples = np.empty((channels, 0), np.double)
     is_listening = False
     is_recording = False
     recording_start = None
     recording_frames = None
-    rewind_buffer_size = int((input_cfg['sample_rate'] * rec_cfg['rewind_ms'] / 1000) / CHUNK_SIZE) * CHUNK_SIZE
-    db_sample_size = int((input_cfg['sample_rate'] * detect_cfg['db_sample_window_ms'] / 1000) / CHUNK_SIZE) * CHUNK_SIZE
+    rewind_buffer_size = int((sample_rate * rec_cfg['rewind_ms'] / 1000) / NUM_FRAMES) * NUM_FRAMES
+    db_sample_size = int((sample_rate * detect_cfg['db_sample_window_ms'] / 1000) / NUM_FRAMES) * NUM_FRAMES
     full_buffer_size = max(rewind_buffer_size, db_sample_size)
-    chunk_ms = CHUNK_SIZE * width * 1000 / input_cfg['sample_rate']
+    chunk_ms = NUM_FRAMES / sample_rate * 1000
     time_loud = 0
     time_quiet = 0
 
     print(f'Buffering...')
     while True:
-        raw_data = stream.read(CHUNK_SIZE, exception_on_overflow=False)
-        data = np.frombuffer(raw_data, dtype=np.int16).reshape((input_cfg['channels'], CHUNK_SIZE), order='F')
+        raw_data = stream.read(NUM_FRAMES, exception_on_overflow=False)
+        data = np.frombuffer(raw_data, dtype=bit_depth_dtype).reshape((channels, NUM_FRAMES), order='F')
         cur_buffer_size = len(buffer[0])
         if cur_buffer_size >= full_buffer_size:
-            buffer = buffer[:, -full_buffer_size+CHUNK_SIZE:]
+            buffer = buffer[:, -full_buffer_size+NUM_FRAMES:]
             if not is_listening:
                 print(f'Listening...')
                 is_listening = True
         buffer = np.append(buffer, data, axis=1)
 
         if cur_buffer_size == full_buffer_size:
-            db = get_rms_db_for_chunk(buffer[detect_cfg['detect_channel'] - 1][-db_sample_size:], width)
+            db = get_rms_dbfs_for_chunk(buffer[detect_cfg['detect_channel'] - 1][-db_sample_size:].tobytes(), width)
             if is_recording:
                 recording_frames += raw_data
             if db > detect_cfg['db_thres']:
@@ -106,9 +133,9 @@ def listen(config):
                     print('Done recording. Saving...')
                     is_recording = False
                     save_path = get_save_path(recording_start, output_cfg['recordings_dir'])
-                    save_frames(recording_frames, input_cfg['channels'], input_cfg['sample_rate'], save_path)
+                    save_frames(recording_frames, channels, sample_rate, pyaudio_format, save_path)
                     recording_dur = timedelta(
-                        milliseconds=len(recording_frames) / (input_cfg['sample_rate'] * width * input_cfg['channels']) * 1000
+                        milliseconds=len(recording_frames) / (sample_rate * width * channels) * 1000
                     )
                     recording_frames = None
                     print(f'Saved to {save_path} with duration of {recording_dur}')
